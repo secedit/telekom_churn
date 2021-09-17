098e27484778472992cb176458b37ef99325cca0
fatih <48352716+secedit@users.noreply.github.com>
2021-09-17 18:04:37 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import MinMaxScaler
 from collections import Counter
 import numpy as np
 
-def preprocess(df, option):
+def preprocess(df):
     """
     This function is to cover all the preprocessing steps on the churn dataframe. It involves selecting important features, encoding categorical data, handling missing values,feature scaling and splitting the data
     """
